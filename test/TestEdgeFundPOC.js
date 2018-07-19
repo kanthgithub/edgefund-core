dfce313a55ac71a98ff4a227a7ac0adcd677a080
Gareth Oates <goatie@gmail.com>
2018-07-19 21:48:07 +0200

    Needed functions to nicely handle blockchain side-effects.
    Closes #2

--- test/TestEdgeFundPOC.js
+++ test/TestEdgeFundPOC.js
@@ -1,3 +1,4 @@
+const helper = require("./helpers/delorean");
 const EdgeFundPOC = artifacts.require("EdgeFundPOC")
 
 contract('testing EdgeFund Contract', async (accounts) =>{
@@ -10,18 +11,12 @@ contract('testing EdgeFund Contract', async (accounts) =>{
         assert.equal(actual, expected);
     });
 
-    // test the eval_mine function
-    it("should advance the blockchain forward a block", (done) =>{
+    it("should advance the blockchain forward a block", async () =>{
         const originalBlockHash = web3.eth.getBlock('latest').hash;
-        let newBlockHash = web3.eth.getBlock('latest').hash;;
+        let newBlockHash = web3.eth.getBlock('latest').hash;
 
-        web3.currentProvider.sendAsync({
-            jsonrpc: "2.0",
-            method: "evm_mine",
-            id: 12345
-            }, (error, result) => {
-                newBlockHash = web3.eth.getBlock('latest').hash;
-                (newBlockHash != originalBlockHash) ? done() : done(new Error("Block not mined"));
-            });
+        newBlockHash = await helper.advanceBlock();
+
+        assert.notEqual(originalBlockHash, newBlockHash);
     });
 })
---
+++ test/helpers/delorean.js
@@ -0,0 +1,39 @@
+function timeTravel(time) {
+    return new Promise((resolve, reject) => {
+        web3.currentProvider.sendAsync({
+            jsonrpc: "2.0",
+            method: "evm_increaseTime",
+            params: [time],
+            id: new Date().getTime()
+        }, (err, result) => {
+            if (err){
+                return reject(err);
+            }
+
+            return resolve(result)
+        });
+    });
+}
+
+function advanceBlock() {
+    return new Promise((resolve, reject) => {
+        web3.currentProvider.sendAsync({
+            jsonrpc: "2.0",
+            method: "evm_mine",
+            id: new Date().getTime()
+        }, (err, result) => {
+            const newBlockHash = web3.eth.getBlock('latest').hash;
+
+            if (err) {
+                return reject(err);
+            }
+
+            return resolve(newBlockHash)
+        });
+    });
+}
+
+module.exports = {
+    timeTravel,
+    advanceBlock
+}
