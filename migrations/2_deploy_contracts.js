0af30c0df773f927a48fa0bb45a891e8262415d0
Gareth Oates <goatie@gmail.com>
2018-07-25 00:42:53 +0200

    Separated cointoss migration in order to be able to release it individually

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,10 @@
 const EdgeFundBettingFunctions = artifacts.require('./EdgeFundBettingFunctions.sol');
 const EdgeFund = artifacts.require("./EdgeFund.sol");
-const CoinToss = artifacts.require("./CoinToss.sol");
 
 module.exports = function(deployer) {
     return deployer.then(async () =>
     {
         await deployer.deploy(EdgeFund);
         await deployer.deploy(EdgeFundBettingFunctions);
-        await deployer.deploy(CoinToss);
     });
 }
---
+++ migrations/3_deploy_cointoss.js
@@ -0,0 +1,8 @@
+const CoinToss = artifacts.require("./CoinToss.sol");
+
+module.exports = function(deployer) {
+    return deployer.then(async () =>
+    {
+        await deployer.deploy(CoinToss);
+    });
+}
