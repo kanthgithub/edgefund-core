66b5233b775e159abff0aaa39cd76845a7bada3d
Gareth Oates <goatie@gmail.com>
2018-07-25 00:04:03 +0200

    Exploring Rinkeby Connection with Infura

--- truffle.js
+++ truffle.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const rinkebyUri = 'https://rinkeby.infura.io/v3/' + process.env.APIKEY;
+
 module.exports = {
     networks: {
         development: {
             host: '127.0.0.1',
             port: 8545,
             network_id: '*' // Match any network id
+        },
+        rinkeby: {
+            provider: function () {
+                return new HDWalletProvider(process.env.MNEMONIC, rinkebyUri)
+            },
+            network_id: 3
         }
     },
     solc: {
@@ -11,5 +22,8 @@ module.exports = {
             enabled: true,
             runs: 200
         }
+    },
+    mocha: {
+        enableTimeouts: false
     }
 };
