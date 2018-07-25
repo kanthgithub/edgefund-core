f6f0122fc216ccd795f6c67c8f1c29343421b827
Gareth Oates <goatie@gmail.com>
2018-07-25 20:16:18 +0200

    Ran out of gas, bumping it up to see if we can get a succesful deploy

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require('./Migrations.sol');
+let Migrations = artifacts.require("./Migrations.sol");
 
-module.exports = function (deployer) {
-    deployer.deploy(Migrations, { gas: 1000000 });
+module.exports = function(deployer) {
+  deployer.deploy(Migrations, {gas: 9000000});
 };
