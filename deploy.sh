c391db116bd78893a280b5884ccd572b7fc3fecb
Gareth Oates <goatie@gmail.com>
2018-08-30 21:39:51 +0200

    Can get address from artifact

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,4 @@
 npm install -g truffle
 
 # 3 refers to migrations/3_deploy_cointoss.js
-export ADDRESS=`truffle migrate -f 3 --network rinkeby | \
-grep "CoinToss" | \
-grep -Eo "0x(\w)*"`
-
-echo $ADDRESS
+export ADDRESS=`truffle migrate -f 3 --network rinkeby
