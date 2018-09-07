b7aecf0dd5d4380b3ff66faca2d17889c8a8d356
Gareth Oates <goatie@gmail.com>
2018-09-07 23:13:56 +0200

    Added npmjs package deployment

--- migrations/2_deploy_edgefund.js
+++ migrations/2_deploy_edgefund.js
@@ -2,10 +2,12 @@ const EdgeFundBettingFunctions = artifacts.require('./EdgeFundBettingFunctions.s
 const EdgeFund = artifacts.require('./EdgeFund.sol');
 const CoinTossMock = artifacts.require('./CoinTossMock.sol');
 
-module.exports = function (deployer) {
-    return deployer.then(async () => {
-        await deployer.deploy(EdgeFund);
-        await deployer.deploy(EdgeFundBettingFunctions);
-        await deployer.deploy(CoinTossMock);
-    });
+module.exports = function (deployer, network) {
+    if (network === 'deployment') {
+        return deployer.then(async () => {
+            await deployer.deploy(EdgeFund);
+            await deployer.deploy(EdgeFundBettingFunctions);
+            await deployer.deploy(CoinTossMock);
+        });
+    }
 };
